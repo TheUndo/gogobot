c60b1cc6ae7a0a090cfcb7206699dbd17f422436
TheUndo <42096971+TheUndo@users.noreply.github.com>
2024-03-25 17:53:45 +0100

    add presence

--- common/client.ts
+++ common/client.ts
@@ -1,4 +1,5 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import { ActivityType, Client, Events, GatewayIntentBits } from "discord.js";
+import { heapStats } from "bun:jsc";
 
 export const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
@@ -6,9 +7,13 @@ export const client = new Client({
 
 client.login(process.env.DISCORD_TOKEN);
 
-new Promise((r) => {
-  client.once(Events.ClientReady, () => {
+await new Promise((r) => {
+  client.once(Events.ClientReady, (c) => {
     console.log("ready.");
     r(undefined);
+    c.user.setActivity({
+      type: ActivityType.Custom,
+      name: `Using ${(heapStats().heapSize / (1024 * 1024)).toFixed(2)} MB of RAM, yum`,
+    });
   });
 });
