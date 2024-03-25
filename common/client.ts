2138ab4a6beee05ffc068a7bfbf5d19d0041fa95
TheUndo <42096971+TheUndo@users.noreply.github.com>
2024-03-25 18:15:51 +0100

    fix presence

--- common/client.ts
+++ common/client.ts
@@ -11,9 +11,42 @@ await new Promise((r) => {
   client.once(Events.ClientReady, (c) => {
     console.log("ready.");
     r(undefined);
-    c.user.setActivity({
-      type: ActivityType.Custom,
-      name: `Using ${(heapStats().heapSize / (1024 * 1024)).toFixed(2)} MB of RAM, yum`,
-    });
   });
 });
+
+function setStatus() {
+  client.user?.setActivity({
+    type: ActivityType.Custom,
+    name: `Using ${formatBytes(heapStats().heapSize)} of RAM, yum`,
+  });
+}
+
+setStatus();
+setInterval(
+  () => {
+    setStatus();
+  },
+  1000 * 60 * 15,
+);
+
+function formatBytes(bytes: number, decimals = 2) {
+  if (!+bytes) return "0 Bytes";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = [
+    "Bytes",
+    "KiB",
+    "MiB",
+    "GiB",
+    "TiB",
+    "PiB",
+    "EiB",
+    "ZiB",
+    "YiB",
+  ];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${Number.parseFloat((bytes / k ** i).toFixed(dm))} ${sizes[i]}`;
+}
