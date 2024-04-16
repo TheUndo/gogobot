971e6695447944ff0a97f76ad94af33686501c26
TheUndo <42096971+TheUndo@users.noreply.github.com>
2024-04-16 22:37:25 +0200

    fix wrong title format

--- interactions/commands/economy/lib/workTitle.ts
+++ interactions/commands/economy/lib/workTitle.ts
@@ -1,8 +1,13 @@
 import { sprintf } from "sprintf-js";
+import { addCurrency } from "~/common/utils/addCurrency";
+import { formatNumber } from "~/common/utils/formatNumber";
 
 export function workTitle(reward: number) {
   if (reward < 0) {
-    return sprintf("You lost $%s", Math.abs(reward));
+    return sprintf(
+      "You lost %s",
+      addCurrency()(formatNumber(Math.abs(reward))),
+    );
   }
-  return sprintf("+$%s", reward);
+  return sprintf("+%s", addCurrency()(formatNumber(reward)));
 }
