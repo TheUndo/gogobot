0440273924a1a1582dc97ae13ed1a7e58ba0a467
TheUndo <42096971+TheUndo@users.noreply.github.com>
2024-04-19 18:36:36 +0200

    improve website

--- website/src/components/button.tsx
+++ website/src/components/button.tsx
@@ -3,8 +3,10 @@ import { Slot, component$ } from "@builder.io/qwik";
 export default component$(
   ({
     href,
+    appearance = "default",
   }: {
     href?: string;
+    appearance?: "default" | "ghost";
   }) => {
     const Cmp = href ? "a" : "button";
     return (
@@ -12,7 +14,10 @@ export default component$(
         role="button"
         href={href}
         type="button"
-        class="py-1 px-3 cursor-pointer hover:bg-gray-100 border-2 border-gray-700 rounded-lg"
+        class={[
+          "py-1 px-3 cursor-pointer hover:bg-gray-100 active:bg-gray-200 border-2 border-gray-700 rounded-lg",
+          appearance === "ghost" && "bg-transparent border-0 hover:underline hover:bg-transparent hover:bg-gray-100",
+        ]}
       >
         <Slot />
       </Cmp>
--- website/src/routes/index.tsx
+++ website/src/routes/index.tsx
@@ -6,15 +6,20 @@ export default component$(() => {
   return (
     <div class="h-full flex justify-center items-center relative">
       <div class="w-[min(100%_-_48px,500px)] relative">
-        <h1 class="text-4xl text-center">GoGoBot</h1>
-        <div class="h-5" />
-        <p class="text-center">Awesome economy/clan/anime Discord bot!</p>
+        <h1 class="font-bold text-4xl text-center">GoGoBot</h1>
+        <div class="h-10" />
+        <p class="text-center">Awesome economy/clan/anime Discord bot</p>
         <div class="h-3" />
-        <div class="flex gap-2 justify-center">
+        <div class="flex justify-center gap-1">
           <Button href="https://discord.com/oauth2/authorize?client_id=805075861463367710">
             Invite bot
           </Button>
-          <Button href="https://discord.gg/kyVfcGuCCQ">Support server</Button>
+          <Button appearance="ghost" href="https://discord.gg/kyVfcGuCCQ">
+            Support server
+          </Button>
+          <Button appearance="ghost" href="https://discord.gg/kyVfcGuCCQ">
+            GitHub
+          </Button>
         </div>
       </div>
     </div>
