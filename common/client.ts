fdad46d776697adae8b3cc405307de159a2c4aff
TheUndo <42096971+TheUndo@users.noreply.github.com>
2024-04-21 17:39:29 +0200

    meta changes

--- common/client.ts
+++ common/client.ts
@@ -32,7 +32,9 @@ setInterval(
 );
 
 function formatBytes(bytes: number, decimals = 2) {
-  if (!+bytes) return "0 Bytes";
+  if (!+bytes) {
+    return "0 Bytes";
+  }
 
   const k = 1024;
   const dm = decimals < 0 ? 0 : decimals;
