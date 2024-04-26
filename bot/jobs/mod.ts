865305da74d3b508eb280cf00216e43e445a5950
TheUndo <42096971+TheUndo@users.noreply.github.com>
2024-04-26 09:39:25 +0200

    changes

--- bot/jobs/mod.ts
+++ bot/jobs/mod.ts
@@ -4,6 +4,9 @@ import { fixClanChannels } from "./fixClanChannels";
 import { fixClanRoles } from "./fixClanRoles";
 import { syncDiscordUsernameCache } from "./syncDiscordUsernameCache";
 
+await fixClanRoles();
+await fixClanChannels();
+
 new CronJob("0 * * * *", aggregateClanStatistics).start();
 new CronJob("0 * * * *", fixClanRoles).start();
 new CronJob("0 0 * * *", fixClanChannels).start();
