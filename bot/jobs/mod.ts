47c86f67a3bb015eb09fe4da259ea94f638291ee
TheUndo <42096971+TheUndo@users.noreply.github.com>
2024-05-03 09:48:12 +0200

    remove immediate job

--- bot/jobs/mod.ts
+++ bot/jobs/mod.ts
@@ -10,5 +10,3 @@ new CronJob("0 * * * *", fixClanRoles).start();
 new CronJob("0 0 * * *", fixClanChannels).start();
 new CronJob("0 * * * *", syncDiscordUsernameCache).start();
 new CronJob("* * * * *", connect4timer).start();
-
-await aggregateClanStatistics();
