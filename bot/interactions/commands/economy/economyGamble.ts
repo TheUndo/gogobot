8f0a574bacf377260cef32c68780c329a36f1996
Martin-404 <168938871+Martin-404@users.noreply.github.com>
2024-05-04 23:35:52 +0100

    Update economyGamble.ts
    DO IT NOW

--- bot/interactions/commands/economy/economyGamble.ts
+++ bot/interactions/commands/economy/economyGamble.ts
@@ -124,7 +124,7 @@ export const gamble = {
       });
     }
 
-    if (bet > 300_000n && interaction.user.name != "Martin") {
+    if (bet > 300_000n && interaction.user.id != 157197838009237504) {
       return await interaction.reply({
         content: "Maximum bet is 300k",
         ephemeral: true,
